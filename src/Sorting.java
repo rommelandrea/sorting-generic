fb5de95d1f97649c311a07f298d4fd02176deef3
Andrea Romanello <rommelandrea@gmail.com>
2011-12-10 17:51:15 +0100

    sistemato quicksort generico

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,10 +1,10 @@
 
 import java.util.Random;
+import java.lang.Comparable.*;
 
 /*
  * Esercitazione 7
  */
-
 /**
  *
  * @author Andrea Romanello 
@@ -16,22 +16,19 @@ public class Sorting {
     public final int SOGLIA = 50;
     private static Random generatore;
 
-    public static <T extends Comparable<T>> void qs(T[] a, int inf, int sup) {
+    private static <T extends Comparable<T>> void qs(T a[], int inf, int sup) {
+        int i, j;
+        i = inf;
+        j = sup;
         if (inf < sup) {
             generatore = new Random();
             int intervallo = generatore.nextInt(sup - inf + 1);    // mi trova un valore compreso tra inf e sup
-            T pivot;
-            pivot = a[intervallo + inf];
-
-            int i, j;
-            i = inf;
-            j = sup;
-
-            while (i <= j) {
-                while (a[i].compareTo(pivot) <= 0 && i <= j) {
+            T pivot = a[intervallo + inf];
+            do {
+                while (a[i].compareTo(pivot) < 0) {
                     i++;
                 }
-                while (a[j].compareTo(pivot) >= 0 && i <= j) {
+                while (a[j].compareTo(pivot) > 0) {
                     j--;
                 }
                 if (i <= j) {
@@ -39,9 +36,12 @@ public class Sorting {
                     i++;
                     j--;
                 }
-            }
-            qs(a, inf, i - 1);
-            qs(a, j + 1, sup);
+            } while (i <= j);
+
+            qs(a, inf, j);
+
+            qs(a, i, sup);
+
         }
     }
 
@@ -74,6 +74,10 @@ public class Sorting {
         }
     }
 
+    public static <T extends Comparable<T>> void qsort(T[] a) {
+        qs(a, 0, a.length - 1);
+    }
+
     public static void qsort(int[] a) {
         qs(a, 0, a.length - 1);
     }
@@ -85,16 +89,16 @@ public class Sorting {
         int a[] = ArrayUtil.randomIntArray(20, 20);
         Integer b[] = ArrayUtil.randomIntegerArray(20, 20);
 
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
         }
 
-        qsort(a);
+        qsort(b);
 
 
         System.out.println("\nstampo l'array ordinato");
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
         }
     }
 }
